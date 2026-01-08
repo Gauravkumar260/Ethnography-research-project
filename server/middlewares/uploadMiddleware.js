f9fd0024341521feaec6e5d7269a1ec81e506eb4
Gaurav kumar <goli.gk000@gmail.com>
2026-01-08 22:52:30 +0530

    Remove multer configuration from uploadMiddleware

--- server/middlewares/uploadMiddleware.js
+++ server/middlewares/uploadMiddleware.js
@@ -124,17 +124,3 @@ module.exports = {
   upload: documentUpload
 };
 
-// Add to uploadMiddleware.js:
-const multer = require('multer');
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'application/pdf') {
-      cb(null, true);
-    } else {
-      cb(new Error('Only PDF files allowed'), false);
-    }
-  }
-});
